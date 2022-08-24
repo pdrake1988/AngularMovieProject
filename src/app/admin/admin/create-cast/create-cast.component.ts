12377031cd5ec043d333f7384f1c231740edfeb2
pdrake1988 <pdrake1@me.com>
2022-08-24 01:26:48 -0500

    added login stuff

---
+++ src/app/admin/admin/create-cast/create-cast.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CastService } from 'src/app/user/services/cast.service';
+
+@Component({
+  selector: 'app-create-cast',
+  templateUrl: './create-cast.component.html',
+  styleUrls: ['./create-cast.component.css']
+})
+export class CreateCastComponent implements OnInit {
+  castInfo = {
+    name: '',
+    gender: '',
+    imdbUrl: ''
+  }
+  constructor(private castService: CastService) { }
+
+  ngOnInit(): void {
+  }
+  createCastMember() {
+    this.castService.
+  }
+}
